c4bad199cc243e256dd9dcf320d922ac14643431
Ryan Spink <ryanspinktech@gmail.com>
2017-02-22 12:59:42 -0700

    add picture url
    add picture url to seed file, add pictures to index, and fixed index format

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,9 @@
 TITLE = ["Yirgacheffe Oromia", "Rushashi Hingakawa", "Namadzi Kapno", "Tarrazu Hacienda La Minita", "Supremo Sur de Huila", "Penachi", "El Triunfo", "Sumatra Aceh", "Atitlan Nahuala", "Mocca Java", "Trieste Caffe" ]
 DESCRIPTION = ["Delicious", "Fantastic", "Unctuous", "Sublime", "Amazing", "Earthy", "Bold", "Deep", "Crisp", "Acidic", "Tart"]
 PRICE = [12, 15, 11, 13, 16, 18, 8, 9, 10]
-IMAGE = ["asdf", "asdfasdf", "asdfasdf", "adsfadf", "adfadsf", "qwetqerh", "qwehhdfskgj"]
+IMAGE = ["http://gbp.com/wp-content/uploads/2014/03/Coffee-Bean-Bag.jpg"]
 
 TITLE.each do |name|
   bean = Bean.create!(title: name, description: DESCRIPTION.sample, price: PRICE.sample, image: IMAGE.sample)
   puts "Created #{bean.title}"
 end
-        
