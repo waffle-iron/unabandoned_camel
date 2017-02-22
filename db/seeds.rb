2f53213e34452dcd55f0af6a59198e742b9210e6
Ryan Spink <ryanspinktech@gmail.com>
2017-02-22 12:40:55 -0700

    add seed file for beans

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+TITLE = ["Yirgacheffe Oromia", "Rushashi Hingakawa", "Namadzi Kapno", "Tarrazu Hacienda La Minita", "Supremo Sur de Huila", "Penachi", "El Triunfo", "Sumatra Aceh", "Atitlan Nahuala", "Mocca Java", "Trieste Caffe" ]
+DESCRIPTION = ["Delicious", "Fantastic", "Unctuous", "Sublime", "Amazing", "Earthy", "Bold", "Deep", "Crisp", "Acidic", "Tart"]
+PRICE = [12, 15, 11, 13, 16, 18, 8, 9, 10]
+IMAGE = ["asdf", "asdfasdf", "asdfasdf", "adsfadf", "adfadsf", "qwetqerh", "qwehhdfskgj"]
+
+TITLE.each do |name|
+  bean = Bean.create!(title: name, description: DESCRIPTION.sample, price: PRICE.sample, image: IMAGE.sample)
+  puts "Created #{bean.title}"
+end
+        
