b03fa1f9ca85de6f57c21eb4d254d17ff1e45e15
Cody Borders <codyborders@gmail.com>
2017-02-22 08:47:04 -0700

    Adds validation spec for Bean model. Will need to add relationship validations in the future.

--- app/models/bean.rb
+++ app/models/bean.rb
@@ -1,2 +1,6 @@
 class Bean < ApplicationRecord
+  validates :title, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :image, presence: true
 end
