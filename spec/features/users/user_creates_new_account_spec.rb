bae58d02dd7b87d578206ace12fa76702fd5fa5a
Cody Borders <codyborders@gmail.com>
2017-02-23 15:20:28 -0700

    Adds new user account feature spec Will need to change page expectations to refect styling and provide more rubust test coverage.

---
+++ spec/features/users/user_creates_new_account_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+
+describe "user creates new account" do
+  scenario "user can see logged-in user dashboard" do
+
+    visit login_path
+
+    click_on "Create Account"
+
+    fill_in "user[email]", with: "bob@aol.com"
+    fill_in "user[password]", with: "password"
+    fill_in "user[password_confirmation]", with: "password"
+
+    click_on "Submit New Account"
+
+    expect(current_path).to eq("/dashboard")
+    expect(page).to have_content("Logged in as #{User.last.email}")
+    expect(page).to have_content("bob@aol.com")
+    expect(page).to have_link("Logout")
+    expect(page).not_to have_link("Login")
+  end
+end
+
