9d3767dd8c10fb741ffeff28e9082ddaed43d6c2
Andy Wooten <andrewdwooten@icloud.com>
2017-02-27 14:41:02 -0700

    Finish admin viewing order test
    Modified test to comply with new requirements for user
    attributes. Applied css id tags to admin order show page
    to allow for more specific targeting of page content.
    Test currently passing.

--- spec/features/admin/admin_views_a_single_order_spec.rb
+++ spec/features/admin/admin_views_a_single_order_spec.rb
@@ -9,7 +9,13 @@ feature 'admin views a single order' do
     bean2 = create(:bean)
     cart = Cart.new({"#{bean1.id}" => 3,
                      "#{bean2.id}" => 2})
-    user = User.create(email: 'test@test.com', password: 'password')
+    user = User.create(email: 'test@test.com', password: 'password',
+                       first_name: 'test_first_name',
+                       last_name: 'test_last_name',
+                       street: '123 Test Ave',
+                       city: 'Test City',
+                       state: 'Test State',
+                       zip: '12345')
     order = user.orders.create(contents: cart.contents,
                                total_price: cart.total_price)
 
@@ -17,19 +23,62 @@ feature 'admin views a single order' do
 
     visit admin_order_path(order)
 
-    expect(page).to have_content("#{bean1.title}")
-    expect(page).to have_content('3')
-    expect(page).to have_content("$#{bean1.subtotal(3)}")
-    expect(page).to have_content("#{bean2.title}")
-    expect(page).to have_content('2')
-    expect(page).to have_content("#{bean2.subtotal(2)}")
-    expect(page).to have_content('test_name')
-    expect(page).to have_content('test_street_and_number')
-    expect(page).to have_content('test_city')
-    expect(page).to have_content('test_state')
-    expect(page).to have_content('test_zipcode')
-    expect(page).to have_content(order.total_price)
-    expect(page).to have_content(order.status)
+
+    within all('#item-link').first do
+      expect(page).to have_link("#{bean1.title}")
+    end
+
+    within all('#item-link').last do
+      expect(page).to have_link("#{bean2.title}")
+    end
+
+    within all('#item-quantity').first do
+      expect(page).to have_content("3")
+    end
+
+    within all('#item-quantity').last do
+      expect(page).to have_content("2")
+    end
+
+    within all('#item-subtotal').first do
+      expect(page).to have_content("$#{bean1.subtotal(3)}")
+    end
+
+    within all('#item-subtotal').last do
+      expect(page).to have_content("$#{bean2.subtotal(2)}")
+    end
+
+    within('#order-total') do
+      expect(page).to have_content(order.total_price)
+    end
+
+    within('#order-status') do
+      expect(page).to have_content(order.status)
+    end
+
+    within('#order-created-date') do
+      expect(page).to have_content(order.date)
+    end
+
+    within ('#user_name') do
+      expect(page).to have_content('test_first_name')
+      expect(page).to have_content('test_last_name')
+    end
+
+    within ('#user_address') do
+      expect(page).to have_content('123 Test Ave')
+      expect(page).to have_content('Test City')
+      expect(page).to have_content('Test State')
+      expect(page).to have_content('12345')
+    end
+
+    within ('#order-status') do
+      expect(page).to have_content(order.status)
+    end
+
+    within ('#order-total') do
+      expect(page).to have_content(order.total_price)
+    end
 
   end
 end
