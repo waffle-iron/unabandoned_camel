d5fc7e4c94a82427334eabb774c76d37b2bae29c
Andy Wooten <andrewdwooten@icloud.com>
2017-03-01 20:25:42 -0700

    Change production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -45,7 +45,7 @@ Rails.application.configure do
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
   config.log_level = :debug
-
+  config.assets.digest = true
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
 
