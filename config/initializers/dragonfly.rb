715fd5b605c09ca69d963f6e731687b0b510140a
Andy Wooten <andrewdwooten@icloud.com>
2017-03-01 20:07:50 -0700

    Change dragonfly.rb

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -5,7 +5,7 @@ Dragonfly.app.configure do
   plugin :imagemagick
 
   secret "018f94545a56f40890077e89ddc845fb3f3a3c1b011a18c81314fdf2ec1a5a01"
-
+  url_host 'http://aqueous-tundra-60091.herokuapp.com'
   url_format "/media/:job/:name"
 
 if Rails.env.development? || Rails.env.test?
@@ -26,7 +26,6 @@ end
 Dragonfly.logger = Rails.logger
 
 # Mount as middleware
-Rails.application.middleware.use Dragonfly::Middleware
 
 # Add model functionality
 if defined?(ActiveRecord::Base)
