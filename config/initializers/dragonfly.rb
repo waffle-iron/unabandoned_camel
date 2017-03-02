fc690d7f0e832bc2dc98ab37f2cacf1e84bb4e7f
Andy Wooten <andrewdwooten@icloud.com>
2017-03-01 21:25:30 -0700

    Add secret for dragon

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -4,6 +4,9 @@ require 'dragonfly/s3_data_store'
 Dragonfly.app.configure do
   plugin :imagemagick
 
+  secret "018f94545a56f40890077e89ddc845fb3f3a3c1b011a18c81314fdf2ec1a5a01"
+  url_host 'http://aqueous-tundra-60091.herokuapp.com'
+
 if Rails.env.development? || Rails.env.test?
   datastore :file,
             root_path: Rails.root.join('public/system/dragonfly', Rails.env),
@@ -13,9 +16,7 @@ else
             bucket_name: ENV['BUCKET_NAME'],
             access_key_id: ENV['S3_KEY'],
             secret_access_key: ENV['S3_SECRET'],
-            url_scheme: 'https',
-            region: "us-west-2"
-
+            url_scheme: 'https'
 end
 
 end
